dbd20d088a9af9bcdc28b0cef13695f14affb8fa
tamilventhan <78096762+tamilventhan@users.noreply.github.com>
2021-05-10 18:32:21 +0530

    Update index.blade.php

--- resources/views/admin/appointments/index.blade.php
+++ resources/views/admin/appointments/index.blade.php
@@ -106,7 +106,7 @@
             // page is now ready, initialize the calendar...
             $('#calendar').fullCalendar({
                 // put your options and callbacks here
-                defaultView: 'agendaWeek',
+                defaultView: 'agendadAY',
                 events : [
                         @foreach($appointments as $appointment)
                     {
